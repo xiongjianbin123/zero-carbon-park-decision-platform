7b242797d6de9ee8e9f5b8d7957e300873c36d20
xiongjianbin123 <xiongjianbin@gmail.com>
2026-08-26 14:53:49 +0800

    test: serialize browser acceptance runs

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, devices } from '@playwright/test'
 export default defineConfig({
   testDir: './tests/e2e',
   fullyParallel: false,
+  workers: 1,
   reporter: 'list',
   use: {
     baseURL: 'http://127.0.0.1:4173',
